c72fe66305ee9243f801d256bcdee032101b465f
Cathleen Turner <turner.cathleen@gmail.com>
2020-09-19 17:28:48 -0400

    added limit to number of requests to process

--- Makefile
+++ Makefile
@@ -2,5 +2,5 @@
 test-multi:
 	for i in `seq 1 1000`; \
 	do\
-		curl  localhost:3000/status;\
+		curl  localhost:3000/cache;\
 	done;\
--- main.go
+++ main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
-    "net/http"
+	"net/http"
+	"fmt"
 )
 
+const maxConnections = 1
+
+func payloadHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		w.Write([]byte("OK"))
+	case "PUT":
+		w.Write([]byte("OK"))
+	case "POST":
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	case "DELETE":
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+// limitNumClients is HTTP handling middleware that ensures no more than
+// maxClients requests are passed concurrently to the given handler f.
+func limitNumClients(f http.HandlerFunc, maxClients int) http.HandlerFunc {
+	// Counting semaphore using a buffered channel
+	sema := make(chan struct{}, maxClients)
+
+	return func(w http.ResponseWriter, req *http.Request) {
+	  sema <- struct{}{}
+	  defer func() { <-sema }()
+	  f(w, req)
+	}
+  }
+
 func main() {
-    mux := http.NewServeMux()
-    mux.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("OK"))
-    })
-    http.ListenAndServe(":3000", mux)
+	mux := http.NewServeMux()
+	// limit to maxConnections for this handler
+    mux.HandleFunc("/cache", limitNumClients(payloadHandler,maxConnections))
+	http.ListenAndServe(":3000", mux)
 }
